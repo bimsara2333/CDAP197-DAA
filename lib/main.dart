6e395ce99409944fa0dadae8c9df32a0a50c61f8
Archana Wijerathne <archanaranushi@gmail.com>
2025-03-18 06:57:19 +0530

    sound disabled

--- lib/main.dart
+++ lib/main.dart
@@ -110,7 +110,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',9999
+        tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
