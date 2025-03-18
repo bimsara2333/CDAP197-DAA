e073952f7a8a7c13955406a2d6cbe0b57a2ab8d7
chathuniN <it21365300@my.sliit.lk>
2025-03-18 05:57:33 +0530

    title changes

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Drive Appl',
+      title: 'Drive App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
